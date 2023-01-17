8320527477169479de1fc5b6a1506ceae2d88307
Rafael Leites Luchese <rlluchese@gmail.com>
2023-01-17 15:15:55 -0300

    Reusable button component

---
+++ comps/src/App.js
@@ -0,0 +1,48 @@
+import Button from './components/Button';
+import { GoBell, GoCloudDownload, GoDatabase } from 'react-icons/go';
+
+function App() {
+  const handleClick = () => {
+    console.log('Click');
+  };
+
+  return (
+    <div>
+      <div>
+        <Button success className='mb-5' onClick={ handleClick }>
+          <GoBell />
+          Success
+        </Button>
+      </div>
+      <div>
+        <Button success outline rounded>
+          <GoCloudDownload />
+          Success
+        </Button>
+      </div>
+      <div>
+        <Button success rounded>
+          <GoDatabase />
+          Success
+        </Button>
+      </div>
+      <div>
+        <Button success outline>Success</Button>
+      </div>
+      <div>
+        <Button primary>Primary</Button>
+      </div>
+      <div>
+        <Button secondary>Secondary</Button>
+      </div>
+      <div>
+        <Button warning>Warning</Button>
+      </div>
+      <div>
+        <Button danger>Danger</Button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
