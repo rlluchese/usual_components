9632df14155526fcb5e3adcdb26886842f973aa5
Rafael Leites Luchese <rlluchese@gmail.com>
2023-02-07 14:22:27 -0300

    Static table
    Table statc for demo purpose

---
+++ comps/src/components/Table.js
@@ -0,0 +1,28 @@
+function Table({ data }) {
+    const renderedRows = data.map((item) => {
+        return (<tr key={ item.name } className="border-b">
+                <td className="p-3">{ item.name }</td>
+                <td className="p-3">
+                    <div className={`p-3 m-2 ${item.color}`}></div>
+                </td>
+                <td className="p-3">{ item.score }</td>
+            </tr>);
+    });
+
+    return (
+        <table className="table-auto border-spacing-2">
+            <thead>
+                <tr className="border-b-2">
+                    <th>Fruit</th>
+                    <th>Color</th>
+                    <th>Score</th>
+                </tr>
+            </thead>
+            <tbody>
+                { renderedRows }
+            </tbody>
+        </table>
+    );
+}
+
+export default Table;
